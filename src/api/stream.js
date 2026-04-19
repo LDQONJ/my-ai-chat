10bde409411e9efa69959f5fe9ecded20c59c6e5
Theodore <490513394dong@gmail.com>
2026-04-19 21:24:03 +0800

    格式化

--- src/api/stream.js
+++ src/api/stream.js
@@ -1,107 +1,104 @@
-import axios from 'axios';
+import axios from 'axios'
 export async function streamChat(messages, onChunk, think = false, signal) {
-    console.log(messages)
-    const userMessage = messages.filter(msg => msg.role === 'user')
-    if (!userMessage.length) return
-    const host = import.meta.env.VITE_API_HOST;
-    const token = localStorage.getItem('token');
-    const headers = {};
-    if (token) {
-        headers['Authorization'] = `${token}`;
-    }
+  console.log(messages)
+  const userMessage = messages.filter(msg => msg.role === 'user')
+  if (!userMessage.length) return
+  const host = import.meta.env.VITE_API_HOST
+  const token = localStorage.getItem('token')
+  const headers = {}
+  if (token) {
+    headers['Authorization'] = `${token}`
+  }
 
-    const res = await fetch(`${host}/chat`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            ...headers
-        },
-        body: JSON.stringify({
-            sessionId: localStorage.getItem('sessionId'),
-            text: userMessage[userMessage.length - 1].content,
-            think
-        }),
-        signal
-    })
+  const res = await fetch(`${host}/chat`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      ...headers,
+    },
+    body: JSON.stringify({
+      sessionId: localStorage.getItem('sessionId'),
+      text: userMessage[userMessage.length - 1].content,
+      think,
+    }),
+    signal,
+  })
 
-    const reader = res.body.getReader()
-     
-    const decoder = new TextDecoder('utf-8')
+  const reader = res.body.getReader()
 
-    let buffer = ''
+  const decoder = new TextDecoder('utf-8')
 
-    while (true) {
-        const { done, value } = await reader.read()
-        if (done) break
+  let buffer = ''
 
-        buffer += decoder.decode(value, { stream: true })
+  while (true) {
+    const { done, value } = await reader.read()
+    if (done) break
 
-        const lines = buffer.split('\n')
-        buffer = lines.pop()
+    buffer += decoder.decode(value, { stream: true })
 
-        for (const line of lines) {
-            if (!line.trim()) continue
+    const lines = buffer.split('\n')
+    buffer = lines.pop()
 
-            try {
-                const jsonLine = line.substring(5).trim()
-                // console.log(jsonLine)
+    for (const line of lines) {
+      if (!line.trim()) continue
 
-                const json = JSON.parse(jsonLine)
+      try {
+        const jsonLine = line.substring(5).trim()
+        // console.log(jsonLine)
 
-                if (json.content) {
-                    onChunk(json)
-                }
-            } catch (e) {
-                console.warn('parse error', e)
-            }
+        const json = JSON.parse(jsonLine)
+
+        if (json.content) {
+          onChunk(json)
         }
+      } catch (e) {
+        console.warn('parse error', e)
+      }
     }
+  }
 }
 
 export async function generateTitle(id, onChunk) {
-    const host = import.meta.env.VITE_API_HOST;
-    const token = localStorage.getItem('token');
-    const headers = {};
-    if (token) {
-        headers['Authorization'] = `${token}`;
-    }
-    const res = await fetch(`${host}/session/title/${id}`, {
-        headers: {
-            ...headers
+  const host = import.meta.env.VITE_API_HOST
+  const token = localStorage.getItem('token')
+  const headers = {}
+  if (token) {
+    headers['Authorization'] = `${token}`
+  }
+  const res = await fetch(`${host}/session/title/${id}`, {
+    headers: {
+      ...headers,
+    },
+  })
+  const reader = res.body.getReader()
+  const decoder = new TextDecoder('utf-8')
+
+  let buffer = ''
+
+  while (true) {
+    const { done, value } = await reader.read()
+    if (done) break
+
+    buffer += decoder.decode(value, { stream: true })
+
+    const lines = buffer.split('\n')
+    buffer = lines.pop()
+
+    for (const line of lines) {
+      if (!line.trim()) continue
+
+      try {
+        const jsonLine = line.substring(5).trim()
+        // console.log(jsonLine)
+
+        const json = JSON.parse(jsonLine)
+
+        if (json.content) {
+          onChunk(json)
         }
-    })
-    const reader = res.body.getReader()
-    const decoder = new TextDecoder('utf-8')
-
-    let buffer = ''
-
-    while (true) {
-        const { done, value } = await reader.read()
-        if (done) break
-
-        buffer += decoder.decode(value, { stream: true })
-
-        const lines = buffer.split('\n')
-        buffer = lines.pop()
-
-        for (const line of lines) {
-            if (!line.trim()) continue
-
-            try {
-                const jsonLine = line.substring(5).trim()
-                // console.log(jsonLine)
-
-                const json = JSON.parse(jsonLine)
-
-                if (json.content) {
-                    onChunk(json)
-                }
-            } catch (e) {
-                console.warn('parse error', e)
-            }
-        }
+      } catch (e) {
+        console.warn('parse error', e)
+      }
     }
-
-
-
+  }
 }
